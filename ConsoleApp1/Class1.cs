3ee91762e0fc5555cc2f1f4e3e3f2a54ebef8a45
алина алина <fkbyf-2003zv@mail.ru>
2021-06-09 15:00:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Root
+    {
+        public string @this { get; set; }
+        public string that { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿// OpenWeatherMap REST API example
+// DKY 2021
+
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ApiKey = "Your API Key";
+            var City = "Kazan";
+            var url = $"http://itsthisforthat.com/api.php?json";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                //Console.WriteLine(result);
+                var weatherForecast = JsonConvert.DeserializeObject<Root>(result);
+                Console.WriteLine(weatherForecast.@this);
+            }
+            Console.ReadLine();
+        }
+    }
+}
